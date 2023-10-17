6a04c847f2d11dd7144e07990401d575ac8c0281
Ryan <ryankane180@gmail.com>
2023-10-17 16:01:06 +0100

    First script implementation. Rudimentary button to add form input to page. WIP

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,10 +1,55 @@
-const submitBtn = document.querySelector("#submit-button");
-const userName = document.querySelector('#username');
-const comment = document.querySelector('#comment');
+// const button = document.querySelector("button");
 
-submitBtn.addEventListener('click', testPrint);
 
-function testPrint(){
-    console.log(userName);
-    console.log(comment)
-}
+
+// function promptTest() {
+//     const comment = prompt("What is your comment?");
+    
+//     console.log("Testing user input");
+
+//     const commentHolder = document.querySelector("#comment_holder");
+//     commentHolder.textContent = `${comment}`;
+
+// }
+
+// button.addEventListener("click", promptTest);
+
+const form = document.getElementById('comment_form');
+const userName = form.elements['username'];
+const comment = form.elements['comment'];
+const commentContainer = document.getElementById('comment_holder');
+
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    let enterredUsername = userName.value;
+    let enterredComment = comment.value;
+    console.log(enterredUsername);
+    console.log(enterredComment);
+    commentContainer.textContent = enterredUsername + "\n" + enterredComment;
+
+});
+
+
+
+
+
+// const login = prompt("What is your login?");
+
+// if (login == "Admin"){
+//     const password = prompt("Please enter your password");
+
+//     if(password == "TheMaster"){
+//         alert("Welcome!");
+//     } else if (password == '' || password == null){
+//         alert("Cancelled");
+//     } else {
+//         alert("Wrong Password");
+//     }
+
+// } else if (login == '' || login == null ) {
+//     alert("Cancelled");
+// } else {
+//     alert("I don't know you");
+// }
