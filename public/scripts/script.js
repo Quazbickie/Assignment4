e15f18303c6435df2518d0f5583b49b6159fbd83
Ryan <ryankane180@gmail.com>
2023-10-22 17:58:47 +0100

    Reformat for mobile. Change steps to two tables, one for each food section

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -8,7 +8,6 @@ let commentContainer = document.getElementById('comment_holder');
 let commentArray = [];
 
 
-
 form.addEventListener("submit", function(event){
     event.preventDefault();
 
@@ -77,10 +76,11 @@ form.addEventListener("submit", function(event){
             commentContainer.appendChild(commentArray[i]);
         }
 
-
         form.reset();
     }
 
+    //return created Time Stamp
+    //getMonth is cast to number and incremented by 1 as it returns a string 0-11
     function getTimeStamp() {
         let dateStr = "";
         let date = new Date();
@@ -92,83 +92,6 @@ form.addEventListener("submit", function(event){
 
 });
 
-
-//TODO: 
-// - Dark Mode Toggle
-// - Perform Date calc in function
-// - form validation
-// - assignment part 1+2/
-
-
-//Array seems to update to contain the same thing in every index
-//is this because the array only has a reference to the item instead of the item itself?
-//need to have this explained to me.
-
-// const form = document.getElementById('comment_form');
-// let commentContainer = document.getElementById('comment_holder');
-// let commentArray = [];
-// let commentBox = document.createElement("div");
-// let commentUser = document.createElement("ul");
-// let commentBody = document.createElement("p");
-// let listUser = document.createElement("li");
-// let listTimeStamp = document.createElement("li");
-// listUser.id = 'user-name';
-
-
-// form.addEventListener("submit", function(event){
-//     event.preventDefault();
-
-//     let userName = form.elements['username'];
-//     let comment = form.elements['comment'];
-//     let enterredUsername = userName.value;
-
-//     if(enterredUsername == "hacker"){
-//         alert("You are banned!");
-//         form.reset();
-//     } else {
-
-//         //need to remove the comments array of Div comments
-//         //just because our comments need to be looped through
-//         while (commentContainer.firstChild) {
-//             commentContainer.removeChild(commentContainer.firstChild);
-//         }
-
-//         listUser.textContent = enterredUsername;
-
-//         //create timestamp
-//         //want to reinitialise this every time
-//         //to get accurate timestamp
-//         let dateStr = "";
-//         let date = new Date();
-//         dateStr += date.getDate() + "-";
-//         let month = Number(date.getMonth());
-//         month++;
-//         dateStr += month + "-";
-//         dateStr += date.getFullYear();
-//         listTimeStamp.textContent = dateStr;
-        
-        
-//         commentUser.appendChild(listUser);
-//         commentUser.appendChild(listTimeStamp);
-
-//         commentBody.textContent = comment.value;
-
-//         commentBox.appendChild(commentUser);
-//         commentBox.appendChild(commentBody);
-//         commentArray.push(commentBox);
-
- 
-//         for(let i = 0; i < commentArray.length; i++){
-//             console.log(commentArray[i]);
-//             commentContainer.appendChild(commentArray[i]);
-//         }
-
-
-//         form.reset();
-//     }
-    
-
-// });
 
 
 
