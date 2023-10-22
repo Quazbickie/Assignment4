be6d3e28291e2f7862dbd3e2f4247ae01b203442
Ryan <ryankane180@gmail.com>
2023-10-22 17:41:50 +0100

    now getTimeStamp in a separate function. Trim enterred username for better handling of prohibited usernames. Add comments

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,5 @@
 //TODO: 
 // - Dark Mode Toggle
-// - Perform Date calc in function
 // - form validation
 // - assignment part 1+2/
 
@@ -13,58 +12,66 @@ let commentArray = [];
 form.addEventListener("submit", function(event){
     event.preventDefault();
 
-    let commentBox = document.createElement("div");
-    let listTimeStamp = document.createElement("li");
-    let commentUser = document.createElement("ul");
-    let listUser = document.createElement("li");
-    let commentBody = document.createElement("p");
-    let hr = document.createElement("hr");
+    let commentDiv = document.createElement("div");         // I am appending a large Div containing Username and comments to the DOM
+    let commentUser = document.createElement("ul");         // Each comment is made up of a username and comment section
+    let listTimeStamp = document.createElement("li");       // the username is in the form of a list, with Username and Timestamp list items
+    let listUser = document.createElement("li");            // the comment is stored in a paragraph element
+    listUser.id = 'user-name';                              // a <hr> is used between the username list and the comment paragraph to provide 
+    let commentBody = document.createElement("p");          // some simply styling.
+    let hr = document.createElement("hr");                  
 
-
-    listUser.id = 'user-name';
-
-
-    let userName = form.elements['username'];
+    //assign the username and comment fields to 
+    //corresponding variables
+    //this variable stores the html element
+    //(i.e) userName holds <input/>
+    let userName = form.elements['username'];  
     let comment = form.elements['comment'];
-    let enterredUsername = userName.value;
 
-    if(enterredUsername == "hacker"){
+    //variable to hold textcontent of username
+    //trim any trailing whitespace.
+    let enterredUsername = userName.value.trim();
+
+    //check if enterred name is hacker, if so alert user
+    //casting tolowercase to make sure 'Hacker' gets caught too
+    //hackers and hACKERS can be very sneaky
+    if(enterredUsername.toLowerCase() == "hacker"){
         alert("You are banned!");
         form.reset();
     } else {
 
         //need to remove the comments array of Div comments
         //just because our comments need to be looped through
+        //previously had simpler solution without use of loop.
         while (commentContainer.firstChild) {
             commentContainer.removeChild(commentContainer.firstChild);
         }
 
+        //listItems to be appended are assigned here
+        //ListUser item is filled with enterred username
+        //listTimeStamp is filled from result of getTimeStamp() function
         listUser.textContent = enterredUsername;
+        listTimeStamp.textContent = getTimeStamp();
 
-        //create timestamp
-        //want to reinitialise this every time
-        //to get accurate timestamp
-        let dateStr = "";
-        let date = new Date();
-        dateStr += date.getDate() + "-";
-        let month = Number(date.getMonth());
-        month++;
-        dateStr += month + "-";
-        dateStr += date.getFullYear();
-        listTimeStamp.textContent = dateStr;
         
-        
+        //append username and timestamp list items
+        //to the commentUser UL item
         commentUser.appendChild(listUser);
         commentUser.appendChild(listTimeStamp);
 
+        //commentBody p item is populated with content
+        //of comment box enterred by user
         commentBody.textContent = comment.value;
 
-        commentBox.appendChild(commentUser);
-        commentBox.appendChild(hr);
-        commentBox.appendChild(commentBody);
-        commentArray.push(commentBox);
+        //commentDiv holder is appended with the Username, followed
+        // by a <hr> and then followed by the comment paragraph
+        //This commentDiv is then added to an array of other comment Divs
+        commentDiv.appendChild(commentUser);
+        commentDiv.appendChild(hr);
+        commentDiv.appendChild(commentBody);
+        commentArray.push(commentDiv);
 
- 
+        //step through comment Divs array and append
+        //each individual comment to the DOM
         for(let i = 0; i < commentArray.length; i++){
             console.log(commentArray[i]);
             commentContainer.appendChild(commentArray[i]);
@@ -72,6 +79,14 @@ form.addEventListener("submit", function(event){
 
 
         form.reset();
+    }
+
+    function getTimeStamp() {
+        let dateStr = "";
+        let date = new Date();
+        dateStr += date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " " + 
+                        date.getDate() + "-" + (Number(date.getMonth())+1) + "-" + date.getFullYear(); 
+        return dateStr;
     }
     
 
